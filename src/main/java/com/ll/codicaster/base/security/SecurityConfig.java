ccc5b0a9353843934736763ca7dc379d0116f61d
daone <jcn07253@gmail.com>
2023-05-31 14:30:09 +0900

    [feat] 카카오, 네이버 로그인 구현
    임시로 내가 사용하는 컴퓨터에서 작동할 수 있도록 application.yml 설정해두었음

--- src/main/java/com/ll/codicaster/base/security/SecurityConfig.java
+++ src/main/java/com/ll/codicaster/base/security/SecurityConfig.java
@@ -2,27 +2,40 @@ package com.ll.codicaster.base.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
-public class SecurityConfig {
+import lombok.RequiredArgsConstructor;
 
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)
+@RequiredArgsConstructor
+public class SecurityConfig {
+    private final CustomOAuth2AccessTokenResponseClient oAuth2AccessTokenResponseClient;
 
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .formLogin(
-                        formLogin -> formLogin
-                                .loginPage("/usr/member/login")
-                )
-
-                .logout(
-                        logout -> logout
-                                .logoutUrl("/usr/member/logout")
-                );
+            .formLogin(
+                formLogin -> formLogin
+                    .loginPage("/usr/member/login")
+            )
+            .oauth2Login(
+                oauth2Login -> oauth2Login
+                    .loginPage("/usr/member/login")
+                    .tokenEndpoint(t -> t
+                        .accessTokenResponseClient(oAuth2AccessTokenResponseClient)
+                    )
+            )
+            .logout(
+                logout -> logout
+                    .logoutUrl("/usr/member/logout")
+            );
 
         return http.build();
     }
