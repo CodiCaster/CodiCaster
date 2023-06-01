a9396ca5c96b6782e88970d5b5a5c031c5dbb424
hojunking96 <hojunking96@gmail.com>
2023-06-01 17:00:56 +0900

    [feat] region Repsitory 생성

---
+++ src/main/java/com/ll/codicaster/boundedContext/region/repository/RegionRepository.java
@@ -0,0 +1,8 @@
+package com.ll.codicaster.boundedContext.region.repository;
+
+import com.ll.codicaster.boundedContext.region.entity.Region;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RegionRepository extends JpaRepository<Region, Long> {
+
+}
