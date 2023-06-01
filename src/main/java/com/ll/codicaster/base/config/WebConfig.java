239abe9896d94bd8241765848949e1935583bac7
Heeesuu <gmltn0508@icloud.com>
2023-06-01 14:13:40 +0900

    [chore] 이미지 처리를 위한 리소스 핸들러 추가

---
+++ src/main/java/com/ll/codicaster/base/config/WebConfig.java
@@ -0,0 +1,14 @@
+package com.ll.codicaster.base.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/images/**")
+			.addResourceLocations("file:///C:/Users/82102/IdeaProjects/CodiCaster-main/images/");
+	}
+}
