3eac1ddd84c639ab50350b9e81789a6cb57ca9f3
hojunking96 <hojunking96@gmail.com>
2023-06-02 15:24:05 +0900

    [feat] 내 정보 페이지 연결 및 기능 구현

--- src/main/java/com/ll/codicaster/boundedContext/member/controller/MemberController.java
+++ src/main/java/com/ll/codicaster/boundedContext/member/controller/MemberController.java
@@ -18,32 +18,39 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/usr/member")
 @RequiredArgsConstructor
 public class MemberController {
-	private final MemberService memberService;
-	private final Rq rq;
+    private final MemberService memberService;
+    private final Rq rq;
 
-	@PreAuthorize("isAnonymous()")
-	@GetMapping("/login")
-	public String showLogin() {
-		return "usr/member/login";
-	}
+    @PreAuthorize("isAnonymous()")
+    @GetMapping("/login")
+    public String showLogin() {
+        return "usr/member/login";
+    }
 
-	@PreAuthorize("isAuthenticated()")
-	@GetMapping("/newInfo")
-	public String showNewInfo() {
-		return "usr/member/newInfo";
-	}
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/newInfo")
+    public String showNewInfo() {
+        return "usr/member/newInfo";
+    }
 
-	@PreAuthorize("isAuthenticated()")
-	@PostMapping("/newInfo")
-	public String updateInfo(@RequestParam String nickname, @RequestParam(required = false) String bodytype, @RequestParam String gender) {
-		memberService.updateMemberInfo(rq.getLoginedMemberId(), nickname, bodytype, gender);
-		return "redirect:/usr/main";
-	}
+    @PreAuthorize("isAuthenticated()")
+    @PostMapping("/newInfo")
+    public String updateInfo(@RequestParam String nickname, @RequestParam(required = false) String bodytype, @RequestParam String gender) {
+        memberService.updateMemberInfo(rq.getLoginedMemberId(), nickname, bodytype, gender);
+        return "redirect:/usr/main";
+    }
 
-	// 중복 확인 요청을 처리하는 핸들러
-	@ResponseBody
-	@GetMapping("/checkNickname")
-	public RsData<String> checkNickname(@RequestParam String nickname) {
-		return memberService.checkNickname(nickname);
-	}
+    // 중복 확인 요청을 처리하는 핸들러
+    @ResponseBody
+    @GetMapping("/checkNickname")
+    public RsData<String> checkNickname(@RequestParam String nickname) {
+        return memberService.checkNickname(nickname);
+    }
+
+    @PreAuthorize("isAuthenticated()") // 로그인 해야만 접속가능
+    @GetMapping("/me") // 로그인 한 나의 정보 보여주는 페이지
+    public String showMe() {
+        return "usr/member/me";
+    }
+
 }
