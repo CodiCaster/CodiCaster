bc948344f18ba7528552436e5e3ae9b93e76d433
Joyfulgwon <101499795+HeegwonJo@users.noreply.github.com>
2023-06-02 14:01:06 +0900

    게시물 추천을 위한 기본 작업 (필터링 및 병합된 코드 정리) (#3)
    * [feat] 게시물 1차 필터링 구현
    
    시간순으로 필터링
    1년전 오늘을 기준으로 앞뒤 한달
    오늘 기준 한달전부터 현재까지 게시물 리스트
    
    * [feat] 유저가 사용한 태그맵, 가장 많이 이용한 태그 확인 기능 구현
    
    * [fix] 이미 추가 정보 입력한경우 정보 입력창으로 이동하지 않게 수정
    
    * [rename] navbar 매핑 수정위한 파일 이름 변경
    
    * [refactor] 매핑 변경
    
    * [feat] 글 작성시 태그리스트 추출기능 구현
    
    * [fix] 연관관계 설정, 게시물 <=> 태그리스트
    
    * [feat] 게시물 작성시 태그맵 태그카운트 증가 구현
    
    * [feat] 전체 게시물 최신순 정렬
    
    * [feat] 태그 중복방지 구현을  위해 리스트 -> Set으로 변경
    
    ---------
    
    Co-authored-by: HeegwonJo <hgzzang33@gmail.com>

--- src/main/java/com/ll/codicaster/boundedContext/member/entity/Member.java
+++ src/main/java/com/ll/codicaster/boundedContext/member/entity/Member.java
@@ -1,15 +1,24 @@
 package com.ll.codicaster.boundedContext.member.entity;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import com.ll.codicaster.base.baseEntity.BaseEntity;
 
+import jakarta.persistence.CollectionTable;
 import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.MapKeyColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
@@ -32,6 +41,13 @@ public class Member extends BaseEntity {
 	private String nickname;
 
 	private String bodytype;
+
+	@ElementCollection
+	@CollectionTable(name = "member_tagMap", joinColumns = @JoinColumn(name = "member_id"))
+	@MapKeyColumn(name = "tag_type")
+	@Column(name = "tag_count")
+	private Map<String, Integer> tagMap;
+
 
 	// 이 함수 자체는 만들어야 한다. 스프링 시큐리티 규격
 	public List<? extends GrantedAuthority> getGrantedAuthorities() {
@@ -56,4 +72,26 @@ public class Member extends BaseEntity {
 		this.nickname = nickname;
 		this.bodytype = bodytype;
 	}
+
+	//유저가 가장 많이 이용한 태그
+	public List<String> getMostUsedTags() {
+		List<String> mostUsedTags = new ArrayList<>();
+		int maxCount = 0;
+
+		Map<String, Integer> tagMap = this.getTagMap();
+
+		for (Map.Entry<String, Integer> entry : tagMap.entrySet()) {
+			int count = entry.getValue();
+			if (count > maxCount) {
+				maxCount = count;
+				mostUsedTags.clear();
+				mostUsedTags.add(entry.getKey());
+			} else if (count == maxCount) {
+				mostUsedTags.add(entry.getKey());
+			}
+		}
+
+		return mostUsedTags;
+	}
+
 }
