e23d19a038937e2636a6ceeaba2da1d56feb2bcf
Joyfulgwon <101499795+HeegwonJo@users.noreply.github.com>
2023-06-11 22:18:53 +0900

    [feat] article 이벤트 리스너 생성

--- src/main/java/com/ll/codicaster/boundedContext/follow/service/FollowService.java
+++ src/main/java/com/ll/codicaster/boundedContext/follow/service/FollowService.java
@@ -1,6 +1,7 @@
 package com.ll.codicaster.boundedContext.follow.service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.context.ApplicationEventPublisher;
