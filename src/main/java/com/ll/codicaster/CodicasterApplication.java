88bafb5fc129c0e7aa994962edcd9085999b44a6
hojunking96 <hojunking96@gmail.com>
2023-06-11 23:35:40 +0900

    [fix] 메인 버튼 오류 해결, CreatedDate, ModifiedDate 저장되도록 수정

--- src/main/java/com/ll/codicaster/CodicasterApplication.java
+++ src/main/java/com/ll/codicaster/CodicasterApplication.java
@@ -2,8 +2,10 @@ package com.ll.codicaster;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class CodicasterApplication {
 
 	public static void main(String[] args) {
