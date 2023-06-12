7dfbcc2a061c91ddffedda363d79485c950f92ac
Dawon <jcn07253@gmail.com>
2023-06-12 16:58:53 +0900

    테스트 코드 추가 및 회원 탈퇴 기능 수정 (#25)
    * [design] 만들어 둔 HTML 파일에 데이지 UI 적용
    
    메뉴 바 아이콘 추가, 문구 한글로 통일하여 수정
    
    * [design] 전체적으로 UI 카드 형식으로 통일
    
    * [feat] 게시글 삭제 버튼 누르면 정말 삭제할 것인지 한번 더 묻는 기능 추가
    
     사용자가 확인을 선택하면 true를 반환하고, 취소를 선택하면 false를 반환함
    
    * [feat] 닉네임 띄어쓰기 제한
    
    공백 있으면 경고 알림 및 가입버튼 비활성화
    
    * [design] me.html 아이콘 수정, 가운데 정렬
    
    * [fix] layout에 todaylist 경로 수정
    
    * [design] newInfo 닉네임 중복 확인 버튼 수정
    
    * [design] 라디오 버튼, 텍스트 인풋, 파일 인풋 등 데이지UI 적용
    
    * [design] 내 정보 페이지 뱃지 색 변경
    
    * [feat] 내 정보 -> 마이페이지로 이름 변경 및 정보 수정 폼 추가
    
    * [fix] 수정 완료 누르면 다시 마이페이지 볼 수 있도록 수정
    
    * [design] UI 개선 및 일관성 있게 수정
    
    * [fix] 기본 정보 입력을 하지 않은 상태로 서비스 이용 못하게 막음
    
    * [fix] 가입하기 페이지의 닉네임 입력 문구 수정
    
    * [feat] 마이페이지에서 회원 탈퇴할 수 있는 기능 추가
    
    * [fix] 탈퇴 버튼에 text-right 적용하여 오른쪽 정렬
    
    * [test] 닉네임 중복 확인 API 테스트 코드 추가
    
    HTTP 상태 코드가 200인지, 메시지가 "사용 가능한 닉네임입니다."라는 문자열과 같은지 확인
    
    * [fix] 오타 수정
    
    * [test] 회원 탈퇴 기능 테스트 추가
    
    memberRepository에서 해당 회원 정보를 먼저 찾고(findById), 그 회원 정보를 삭제(delete)하는지를 검증
    
    * [test] MemberTest 추가
    
    권한 검증, 관리자인지 확인, 정보 업데이트, 가장 많이 사용한 태그 반환, 체형 타입에 따른 이름 확인 테스트
    
    * [fix] 폼 서브밋 방지 기능 추가하여 탈퇴 시 취소 버튼 누르면 정상적으로 취소되도록 수정

---
+++ src/test/java/com/ll/codicaster/member/MemberTest.java
@@ -0,0 +1,88 @@
+package com.ll.codicaster.member;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import com.ll.codicaster.boundedContext.member.entity.Member;
+
+public class MemberTest {
+	private Member member;
+
+	@BeforeEach
+	public void setUp() {
+		Map<String, Integer> tagMap = new HashMap<>();
+		tagMap.put("tag1", 5);
+		tagMap.put("tag2", 5);
+		tagMap.put("tag3", 3);
+
+		member = Member.builder()
+			.providerTypeCode("CODYCASTER")
+			.username("testUser")
+			.password("testPassword")
+			.nickname("testNickname")
+			.bodyType(3)
+			.gender("Male")
+			.tagMap(tagMap)
+			.build();
+	}
+
+	@Test
+	@DisplayName("권한 확인 테스트")
+	public void t001() {
+		List<? extends GrantedAuthority> authorities = member.getGrantedAuthorities();
+
+		// 모든 회원은 "member" 권한을 가집니다.
+		assertTrue(authorities.contains(new SimpleGrantedAuthority("member")));
+
+		// "admin" 사용자는 "admin" 권한도 가집니다.
+		if (member.isAdmin()) {
+			assertTrue(authorities.contains(new SimpleGrantedAuthority("admin")));
+		}
+	}
+
+	@Test
+	@DisplayName("관리자 확인 테스트")
+	public void t002() {
+		assertEquals("admin".equals(member.getUsername()), member.isAdmin());
+	}
+
+	@Test
+	@DisplayName("멤버 정보 업데이트")
+	public void t003() {
+		member.updateInfo("newNickname", 2, "Female");
+
+		assertEquals("newNickname", member.getNickname());
+		assertEquals(2, member.getBodyType());
+		assertEquals("Female", member.getGender());
+	}
+
+	@Test
+	@DisplayName("가장 많이 사용한 태그 확인")
+	public void t004() {
+		List<String> expected = Arrays.asList("tag1", "tag2");
+		List<String> mostUsedTags = member.getMostUsedTags();
+
+		assertEquals(expected, mostUsedTags);
+	}
+
+	@Test
+	@DisplayName("체질에 따른 표시 이름 확인")
+	public void t005() {
+		String expected = "보통";
+		String actual = member.getBodyTypeDisplayName();
+
+		assertEquals(expected, actual);
+	}
+}
+
