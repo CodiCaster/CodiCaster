0e1a718c816cda3d970ded6a3ada8421c8e2a92c
hojunking96 <hojunking96@gmail.com>
2023-06-12 20:50:18 +0900

    [feat] API 기능 분리

--- src/main/java/com/ll/codicaster/boundedContext/location/service/KakaoAPIService.java
+++ src/main/java/com/ll/codicaster/boundedContext/location/service/KakaoAPIService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 
 @Service
@@ -17,43 +18,51 @@ public class KakaoAPIService {
     private String REST_KEY;
 
     public String getAddressFromKakao(double longitude, double latitude) {
-        String address = "";
-        String urlString = "https://dapi.kakao.com/v2/local/geo/coord2regioncode?x=" + longitude + "&y=" + latitude;
-
         try {
-            URL url = new URL(urlString);
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("GET");
-            conn.setRequestProperty("Authorization", "KakaoAK " + REST_KEY);
-
-            // 응답 내용 읽기
-            BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-            String line;
-            StringBuilder response = new StringBuilder();
-            while ((line = reader.readLine()) != null) {
-                response.append(line);
-            }
-            reader.close();
-
-            JSONObject json = new JSONObject(response.toString());
-            JSONArray documents = json.getJSONArray("documents");
-
-            for (int i = 0; i < documents.length(); i++) {
-                JSONObject document = documents.getJSONObject(i);
-
-                if (document.getString("region_type").equals("B")) {
-                    String address1 = document.getString("region_1depth_name");
-                    String address2 = document.getString("region_2depth_name");
-                    String address3 = document.getString("region_3depth_name");
-
-                    address = address1 + " " + address2 + " " + address3;
-                }
-            }
-            // 연결 종료
-            conn.disconnect();
+            URL url = getURL(longitude, latitude);
+            StringBuilder response = getResponse(url);
+            return getAddressFromJSON(response);
         } catch (Exception e) {
             return null;
         }
-        return address;
+    }
+
+    private URL getURL(double longitude, double latitude) throws MalformedURLException {
+        String urlString = "https://dapi.kakao.com/v2/local/geo/coord2regioncode?x=" + longitude + "&y=" + latitude;
+        return new URL(urlString);
+    }
+
+    private StringBuilder getResponse(URL url) throws Exception {
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        conn.setRequestProperty("Authorization", "KakaoAK " + REST_KEY);
+
+        // 응답 내용 읽기
+        BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        String line;
+        StringBuilder response = new StringBuilder();
+        while ((line = reader.readLine()) != null) {
+            response.append(line);
+        }
+        reader.close();
+        conn.disconnect();
+        return response;
+    }
+
+    private String getAddressFromJSON(StringBuilder response) {
+        JSONObject json = new JSONObject(response.toString());
+        JSONArray documents = json.getJSONArray("documents");
+
+        for (int i = 0; i < documents.length(); i++) {
+            JSONObject document = documents.getJSONObject(i);
+
+            if (document.getString("region_type").equals("B")) {
+                String address1 = document.getString("region_1depth_name");
+                String address2 = document.getString("region_2depth_name");
+                String address3 = document.getString("region_3depth_name");
+                return address1 + " " + address2 + " " + address3;
+            }
+        }
+        return null;
     }
 }
