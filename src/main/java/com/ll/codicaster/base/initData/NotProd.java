3b14d9115ec78bea1dced4f126b627067cb81694
daone <jcn07253@gmail.com>
2023-06-13 15:01:22 +0900

    [fix] 자동 생성되는 초기 데이터 부분 삭제

--- src/main/java/com/ll/codicaster/base/initData/NotProd.java
+++ src/main/java/com/ll/codicaster/base/initData/NotProd.java
@@ -29,14 +29,9 @@ public class NotProd {
 			public void run(String... args) throws Exception {
 				Member memberAdmin = memberService.join("admin", "1234").getData();
 				Member memberUser1 = memberService.join("user1", "1234").getData();
-				Member memberUser2 = memberService.join("user2", "1234").getData();
-				Member memberUser3 = memberService.join("user3", "1234").getData();
-				Member memberUser4 = memberService.join("user4", "1234").getData();
-				Member memberUser5 = memberService.join("user5", "1234").getData();
-
-				Member memberUser6ByKakao = memberService.whenSocialLogin("KAKAO",
+				Member memberUserByKakao = memberService.whenSocialLogin("KAKAO",
 					"KAKAO__%s".formatted(kakaoDevUserOAuthId)).getData();
-				Member memberUser8ByNaver = memberService.whenSocialLogin("NAVER",
+				Member memberUserByNaver = memberService.whenSocialLogin("NAVER",
 					"NAVER__%s".formatted(naverDevUserOAuthId)).getData();
 			}
 		};
