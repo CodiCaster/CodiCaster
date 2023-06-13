f77c9bf1d3586b1251aa80fe5156bfac0fd48008
Joyfulgwon <101499795+HeegwonJo@users.noreply.github.com>
2023-06-13 15:01:35 +0900

    [feat] 팔로우한 사람 피드 리스트 생성

--- src/main/java/com/ll/codicaster/boundedContext/article/service/ArticleService.java
+++ src/main/java/com/ll/codicaster/boundedContext/article/service/ArticleService.java
@@ -1,4 +1,3 @@
-
 package com.ll.codicaster.boundedContext.article.service;
 
 import java.io.File;
@@ -54,8 +53,8 @@ public class ArticleService {
 	@Value("${file.upload-dir}")
 	private String uploadDir;
 
-    public static Set<String> extractHashTagList(String content) {
-        Set<String> tagSet = new HashSet<>();
+	public static Set<String> extractHashTagList(String content) {
+		Set<String> tagSet = new HashSet<>();
 
 		Pattern pattern = Pattern.compile("#([ㄱ-ㅎ가-힣a-zA-Z0-9_]+)");
 		Matcher matcher = pattern.matcher(content);
@@ -336,23 +335,23 @@ public class ArticleService {
 		}
 	}
 
-    @Transactional
-    public RsData likeArticle(Member member, Long articleId) {
-        if (member == null) {
-            return RsData.of("F-1", "로그인 후 사용가능한 기능입니다.");
-        }
-        Article article = articleRepository.findById(articleId).orElse(null);
-        if (article == null) {
-            return RsData.of("F-2", "존재하지 않는 게시물입니다.");
-        }
-        article.addLikeMember(member);
-        Set<Member> likeSet = article.getLikedMembers();
-        likeSet.add(member);
-        article.setLikedMembers(likeSet);
-        //이벤트 발행
-        publisher.publishEvent(new EventAfterLike(this, member, article));
-        return RsData.of("S-1", "좋아요가 추가되었습니다.");
-    }
+	@Transactional
+	public RsData likeArticle(Member member, Long articleId) {
+		if (member == null) {
+			return RsData.of("F-1", "로그인 후 사용가능한 기능입니다.");
+		}
+		Article article = articleRepository.findById(articleId).orElse(null);
+		if (article == null) {
+			return RsData.of("F-2", "존재하지 않는 게시물입니다.");
+		}
+		article.addLikeMember(member);
+		Set<Member> likeSet = article.getLikedMembers();
+		likeSet.add(member);
+		article.setLikedMembers(likeSet);
+		//이벤트 발행
+		publisher.publishEvent(new EventAfterLike(this, member, article));
+		return RsData.of("S-1", "좋아요가 추가되었습니다.");
+	}
 
 	@Transactional
 	public RsData unlikeArticle(Member member, Long articleId) {
