e4af48b2c731b57b6fcc213a4610e28b6a1b2742
Dawon <jcn07253@gmail.com>
2023-06-15 16:32:46 +0900

    UI 개선 및 내 게시글 링크 추가 (#38)
    * [remove] 임시로 추가해놓은 html 파일 삭제
    
    * [fix] 관심 게시글 -> 관심 게시물 목록으로 수정
    
    * [design] 팔로우, 팔로워 목록 UI 개선
    
    * [fix] 알림 뱃지 색 수정
    
    * Revert "[fix] 알림 뱃지 색 수정"
    
    This reverts commit 9af9dc77782a8728a14ad3af996497e441e5dd27.
    
    * [fix] 알림 뱃지 색 수정
    
    * [fix] 충돌 해결
    
    * [design] UI 수정
    
    * [fix] 희권님 코드 추가
    
    * [design] 마이페이지, 네비게이션 바에 내 게시글 추가 및 메인 UI 수정

--- src/main/java/com/ll/codicaster/boundedContext/notification/repository/NotificationRepository.java
+++ src/main/java/com/ll/codicaster/boundedContext/notification/repository/NotificationRepository.java
@@ -19,8 +19,4 @@ public interface NotificationRepository extends JpaRepository<Notification, Long
 	List<Notification> findByArticleIdAndActor(Long articleId, Member actor);
 
 	List<Notification> findByArticleId(Long articleId);
-<<<<<<< HEAD
-
-=======
->>>>>>> ed69c83 ([fix] 좋아요 표시 되어있는 게시물 삭제 가능)
 }
